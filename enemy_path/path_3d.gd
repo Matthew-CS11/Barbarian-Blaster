2b3e8338f7e307e6e74eaae824fa67a935b5386a
Matthew-CS11 <2260213@flourbluffschools.org>
2026-01-27 12:39:33 -0600

    add difficulty curve(WIP)

--- enemy_path/path_3d.gd
+++ enemy_path/path_3d.gd
@@ -1,11 +1,14 @@
 extends Path3D
 
 @export var enemy_scene : PackedScene
+@export var diff_manager : Node
+@onready var timer: Timer = $Timer
 
 func spawn_enemy() -> void:
 	var enemy = enemy_scene.instantiate()
 	add_child(enemy)
-
+	timer.wait_time = diff_manager.get_spawn_time()
+	enemy.max_health = diff_manager.get_enemy_health()
 
 func _on_timer_timeout() -> void:
 	spawn_enemy()
